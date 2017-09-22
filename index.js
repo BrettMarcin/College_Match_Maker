833e728e9ae9388e39847fdb15809ee7dd7dde44
BrettMarcin <brettmarcin@gmail.com>
2017-09-22 13:26:31 -0400

    added postgres database support

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
+var Sequelize = require('sequelize');
 
 app.use(express.static(__dirname + '/'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+const sequelize = new Sequelize('postgres://new_user:new_password@localhost:5432/colleges');
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
+
+app.post('/addToDatabase', function(req, res){
+
+});
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/views/home.html'));
